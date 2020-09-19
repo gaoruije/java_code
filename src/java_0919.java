076e00c000bef6a3fe323d06c8ccb211ecaa5fc4
高瑞杰 <3131114593@qq.com>
2020-09-19 22:41:02 +0800

    java基本语法结构2

---
+++ src/java_0919.java
@@ -0,0 +1,15 @@
+public class java_0919 {
+    public static void main(String[] args) {
+        //1,表示字符类型（char）
+        char ch ='A';//注意给字符赋值的时候，要用单引号‘’。
+        //表示整数的（byte）,注意范围，-128~127
+        byte num = 10;//给byte赋值数字的时候，要注意范围。
+        //2，表示短整形变量类型（short）
+        // short a = 67;
+        //char b = a;//short 和 char不能相互转化，否则会报错变红。
+        //3，boolean 类型，只会出现两个结果。true表示真 或者 false表示假
+        //4,字符串类型
+
+        System.out.println(num);
+    }
+}
