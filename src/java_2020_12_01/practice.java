0d74885f3eefa36dfe34dbc11a3c5209da62f7b3
高瑞杰 <3131114593@qq.com>
2020-12-01 21:28:32 +0800

    hello world的输出

---
+++ src/java_2020_12_01/practice.java
@@ -0,0 +1,16 @@
+package java_2020_12_01;
+
+public class practice {//类的名字必须和文件名一样(这是硬性要求)，这里都是practice
+    //java中叫的是main方法，c语言中叫做main函数
+    //1,用java写的第一个hello word程序
+    public static void main(String[] args){//用main加tab键
+        //String[]是一个字符串数组，args是一个形参，咋样起名都行
+        System.out.println("Hello world!");//用sout加tab，带换行
+        //System是一个类名
+        //2,创建一个变量
+        int number = 0;//变量的初始化
+        number = 10;//变量的赋值
+        System.out.println("number = " + number);
+    }
+}
+
