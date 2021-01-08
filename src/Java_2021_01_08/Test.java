51e4a76bfc1462b571f72c983ec0db044f43ef77
高瑞杰 <3131114593@qq.com>
2021-01-08 20:57:29 +0800

    类的实例化

---
+++ src/Java_2021_01_08/Test.java
@@ -0,0 +1,11 @@
+package Java_2021_01_08;
+
+public class Test {
+    public static void main(String[] args) {
+        //对Person中的类进行实例化的操作,实例化的时候，创建的对象可以是多个
+        Person person = new Person();
+        Person person1 = new Person();
+        Person person2 = new Person();
+        Person person3 = new Person();
+    }
+}
