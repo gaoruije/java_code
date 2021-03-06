89b47138202178e34d4c712dfa4061698d69b17e
高瑞杰 <3131114593@qq.com>
2021-03-06 22:47:15 +0800

    链表的创建，求长度，打印

---
+++ src/Java_2021_03_06/createNodeList.java
@@ -0,0 +1,31 @@
+package Java_2021_03_06;
+
+public class createNodeList {
+    public static Node createList() {
+        Node a = new Node(1);
+        Node b = new Node(2);
+        Node c = new Node(3);
+        Node d = new Node(4);
+        Node e = new Node(5);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = null;
+        return a;
+    }
+
+    public static void print(Node head) {
+        for (Node cur = head; cur != null; cur = cur.next) {
+            System.out.println(cur.val);
+        }
+    }
+
+    public static int getLength(Node head) {
+        int count = 0;
+        for (Node cur = head; cur != null; cur = cur.next) {
+            count++;
+        }
+        return count;
+    }
+}
